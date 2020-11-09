2abdc1e2645bdaacd18971dffbf269900cebe541
Girma Ebssa <girma.ebssa@gmail.com>
2020-11-09 00:14:28 -0800

    define classes and set prompt questions

--- Develop/app.js
+++ Develop/app.js
@@ -33,3 +33,117 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+const employees = [];
+
+const  promptQuestions = () => inquirer.prompt([
+    {
+        type: 'input',
+        name: 'managerName',
+        message: "What is your manager's name?"
+    },
+    {
+        type: 'input',
+        name: 'managerId',
+        message: "What is your manager's id?"
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: "What is your manager's email?"
+    },
+    {
+        type: 'input',
+        name: 'managerOffice',
+        message: "What is your manager's office number?"
+    }   
+]);
+
+const addTeamMember = () => inquirer.prompt([
+    {
+        type: 'list',
+        name: 'empType',
+        message: "Which type of team members would you like to add?",
+        choices: ['Engineer', 'Intern', 'I do not want to add more team members']
+    }
+])
+.then((type) => {
+    switch(type.empType){
+        case 'Engineer':
+            promptEngineer()
+            .then((data) =>{
+                const engineer = new Engineer(data.engName, data.engId, data.engEmail, data.engGit)
+                employees.push(engineer)
+                addTeamMember()
+            })
+            break;
+        case 'Intern':
+            promptIntern()
+            .then((data) =>{
+                const intern = new Intern(data.intName, data.intId, data.intEmail, data.intSchool)
+                employees.push(intern)
+                addTeamMember()
+            })
+            break;
+        default:
+            console.log('Thank you for providing infomation')
+    }
+})
+
+const promptEngineer = () => inquirer.prompt([
+    {
+        type: 'input',
+        name: 'engName',
+        message: "What is this Engineer's name?"
+    },
+    {
+        type: 'input',
+        name: 'engId',
+        message: "What is this Engineer's id?"
+    },
+    {
+        type: 'input',
+        name: 'engEmail',
+        message: "What is this Engineer's email?"
+    },
+    {
+        type: 'input',
+        name: 'engGit',
+        message: "What is this Engineer's github user name?"
+    },
+]); 
+
+const promptIntern = () => inquirer.prompt([
+    {
+        type: 'input',
+        name: 'intName',
+        message: "What is this intern's name?"
+    },
+    {
+        type: 'input',
+        name: 'intId',
+        message: "What is this intern's id?"
+    },
+    {
+        type: 'input',
+        name: 'intEmail',
+        message: "What is this intern's email?"
+    },
+    {
+        type: 'input',
+        name: 'intSchool',
+        message: "What is this intern's school?"
+    },
+]);
+
+//====================================//
+console.log(`Please build your team`)
+
+promptQuestions()
+.then((data) => {
+   const manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.managerOffice)
+   employees.push(manager);
+   addTeamMember();
+   console.log(...employees);
+   console.log(employees);
+})
